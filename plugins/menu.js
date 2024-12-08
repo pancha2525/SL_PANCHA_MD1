c003c241cf27afcb47ce1b70d6f7a29a6e13ad08
pancha000 <panchamd2512@gmail.com>
2024-12-08 22:10:28 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 
 cmd({
@@ -9,6 +9,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await readEnv();
 let menu = {
 main: '',
 download: '',
@@ -20,7 +21,7 @@ search:''
 
 for (let i = 0; i < commands.length; i++) {
 if (commands[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `.${commands[i].pattern}\n`;
+menu[commands[i].category] += `${config.PREFIX}${commands[i].pattern}\n`;
  }
 }
 
@@ -51,7 +52,7 @@ ${menu.serch}
 
 👋 *POWERD BY SL_PANCHA_MD WHATSAPP BOT* ✅
 `
-await conn.sendMessage(from,{image:{url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrzKnIhLO5SjLoUg8dGxYxiE7OdqL65l_nXQ&s"},caption:madeMenu},{quoted:mek})
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
 
 
 
