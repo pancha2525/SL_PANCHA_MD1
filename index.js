4b43e0768f878d796b6931aa2f81a85df56d4d52
pancha000 <panchamd2512@gmail.com>
2024-12-08 21:46:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ fetchLatestBaileysVersion,
 Browsers
 } = require('@whiskeysockets/baileys')
 
+
+
+
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson } = require('./lib/functions')
 const fs = require('fs')
 const P = require('pino')
@@ -39,6 +42,10 @@ const port = process.env.PORT || 8000;
 //=============================================
 
 async function connectToWA() {
+//============================connect mongodb ==========================================
+const connectDB = require('./lib/mongodb')
+connectDB();
+//===================================================
 console.log("Connecting sl-pancha-md bot 🧬...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
