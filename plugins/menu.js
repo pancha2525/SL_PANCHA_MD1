28b79d823bf5e5abcdca56126fb1f45e4dd58ce3
pancha2525 <alondra20346@gmail.com>
2024-12-14 19:31:04 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -9,7 +9,6 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-const config = await readEnv();
 let menu = {
 main: '',
 download: '',
